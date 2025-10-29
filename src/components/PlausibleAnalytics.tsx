8ac962fe37f6575fd488f7c3c0cde52dafce4b50
findingsimple <jason@findingsimple.com>
2025-10-30 10:29:40 +1030

    Fix import

--- src/components/PlausibleAnalytics.tsx
+++ src/components/PlausibleAnalytics.tsx
@@ -1,19 +1,13 @@
 'use client'
 
 import { useEffect } from 'react'
-import Plausible from '@plausible-analytics/tracker'
+import { init } from '@plausible-analytics/tracker'
 
 export function PlausibleAnalytics() {
   useEffect(() => {
-    const plausible = Plausible({
+    init({
       domain: process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN || '',
     })
-
-    // Enable automatic page view tracking for SPA
-    const cleanup = plausible.enableAutoPageviews()
-
-    // Cleanup on unmount
-    return cleanup
   }, [])
 
   return null
