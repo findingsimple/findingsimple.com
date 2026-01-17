0743893c1102b8f94b9b83483ddab424b8803e23
findingsimple <jason@findingsimple.com>
2026-01-18 09:37:15 +1030

    Remove CV button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -198,10 +198,6 @@ function Resume() {
           <Role key={roleIndex} role={role} />
         ))}
       </ol>
-      <Button href="https://docs.google.com/document/d/11hqUdT45kOTPYf8sF0gaRq6X3iKCzkB5CH4tyA7EK04/edit?usp=sharing" variant="secondary" className="group mt-6 w-full">
-        Download CV
-        <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
-      </Button>
     </div>
   )
 }
